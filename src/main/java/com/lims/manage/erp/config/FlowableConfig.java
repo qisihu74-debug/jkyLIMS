1290afb344c71ac85c5fcca0c04c050800a9ecc5
gjl <18337165257@163.com>
2021-11-17 16:12:32 +0800

    支持多个bpmn流程启动部署

--- src/main/java/com/lims/manage/erp/config/FlowableConfig.java
+++ src/main/java/com/lims/manage/erp/config/FlowableConfig.java
@@ -1,5 +1,6 @@
 package com.lims.manage.erp.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @Date: Create in 2019/5/20 10:26
  */
 @Configuration
+@Slf4j
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
     @Override
     public void configure(SpringProcessEngineConfiguration springProcessEngineConfiguration) {
@@ -17,4 +19,6 @@ public class FlowableConfig implements EngineConfigurationConfigurer<SpringProce
         springProcessEngineConfiguration.setLabelFontName("宋体");
         springProcessEngineConfiguration.setAnnotationFontName("宋体");
     }
+
+
 }
---
+++ src/main/java/com/lims/manage/erp/config/FlowableRunner.java
@@ -0,0 +1,53 @@
+package com.lims.manage.erp.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.flowable.engine.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.config
+ * @desc
+ * @date 2021/11/17 15:42
+ * @Copyright © 河南交科院
+ */
+@Service
+@Slf4j
+public class FlowableRunner implements ApplicationRunner {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //支持多个bpmn文件自动部署
+        File file = null;
+        ClassPathResource resource = new ClassPathResource("processes" + File.separator);
+        try {
+            file = resource.getFile();
+        }catch (IOException e){
+            log.error("读取流程配置文件失败:{}",e);
+        }
+        File[] files = file.listFiles();
+        if (files.length>1){
+            for (File file1:files) {
+                String path = "processes\\"+file1.getName();
+                try {
+                    repositoryService.createDeployment()
+                            .addClasspathResource(path)
+                            .deploy();
+                    log.info("成功：部署工作流成功：{}",path);
+                } catch (Exception e) {
+                    log.error("失败：部署工作流失败：" + path);
+                }
+            }
+        }
+    }
+}
