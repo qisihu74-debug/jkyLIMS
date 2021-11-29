4baa87567f792480d676cf40e4dd92cf970ce32a
gjl <18337165257@163.com>
2021-11-29 16:00:58 +0800

    提交文件上传不同文件不同桶名的命名

---
+++ src/main/java/com/lims/manage/erp/constant/BucketsConst.java
@@ -0,0 +1,32 @@
+package com.lims.manage.erp.constant;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.constant
+ * @desc 文件桶名常量
+ * @date 2021/11/29 15:49
+ * @Copyright © 河南交科院
+ */
+public class BucketsConst {
+    /**
+     * 报告模板文件通名
+     */
+    public static final String buckets_report = "reportTemplate";
+    /**
+     * 委托单附件资料通名
+     */
+    public static final String buckets_entrust_enclosure = "entrustEnclosure";
+    /**
+     * 样品附件文件通名
+     */
+    public static final String buckets_sample_enclosure = "sampleEnclosure";
+    /**
+     * 样品图片文件通名
+     */
+    public static final String buckets_sample_picture = "samplePicture";
+    /**
+     * 原始记录文件通名
+     */
+    public static final String buckets_original_record = "originalRecord";
+}
