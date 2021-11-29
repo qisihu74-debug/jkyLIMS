b67da6a7a1d8f4f31cf32a93e4c8104701959aa7
psh <psh080808>
2021-11-29 09:53:09 +0800

    添加委托相关内容提交；

---
+++ src/main/java/com/lims/manage/erp/controller/EntrustController.java
@@ -0,0 +1,9 @@
+package com.lims.manage.erp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("entrust")
+public class EntrustController {
+}
---
+++ src/main/java/com/lims/manage/erp/service/impl/EntrustServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lims.manage.erp.service.impl;
+
+import com.lims.manage.erp.service.EntrustService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EntrustServiceImpl implements EntrustService {
+}
