41dd55e7bc70e406fc41c5bc2ec4f4a60bd2a9ba
gjl <18337165257@163.com>
2022-02-23 17:33:06 +0800

    对接契约锁接口1

--- src/main/java/com/lims/manage/erp/mapper/TaskMapper.java
+++ src/main/java/com/lims/manage/erp/mapper/TaskMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lims.manage.erp.entity.*;
 import com.lims.manage.erp.vo.*;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -177,4 +179,11 @@ public interface TaskMapper extends BaseMapper {
      */
     int updatePersonInfo(PersonInfoVo vo);
 
+    /**
+     * 根据任务id获取委托单id
+     * @param id
+     * @return
+     */
+    @Select("select entrustment_id from test_task where id = #{id}")
+    Long getEntrustIdByTaskId(@Param("id") Long id);
 }
