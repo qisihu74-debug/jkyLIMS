27d770562fd02590796955cf88ec8b85833db030
gjl <18337165257@163.com>
2022-03-28 11:33:49 +0800

    默认访问页设置

--- src/main/java/com/lims/manage/erp/config/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/lims/manage/erp/config/MyWebMvcConfigurerAdapter.java
@@ -2,13 +2,16 @@ package com.lims.manage.erp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
+public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         //允许任何域名
@@ -27,4 +30,5 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
         source.registerCorsConfiguration("/**", buildConfig());
         return new CorsFilter(source);
     }
+
 }
