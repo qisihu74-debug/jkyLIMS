e6a0c810306adcf845c27516a37122b1d6ef2b1a
psh <psh080808>
2022-04-07 09:09:14 +0800

    样品管理--样品签收新增样品接口添加；

---
+++ src/main/java/com/lims/manage/erp/entity/TestSampleEntity.java
@@ -0,0 +1,97 @@
+package com.lims.manage.erp.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.lims.manage.erp.vo.SampleDetailAddVo;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName("test_sample")
+public class TestSampleEntity {
+    private Integer id;
+
+    private Integer companyId;
+
+    private String sampleName;
+
+    private String sampleCode;
+
+    private String specs;
+
+    private String batchNumber;
+
+    private String manufacturer;
+
+    private String sampleOrigin;
+
+    private String outward;
+
+    private String savePlace;
+
+    private String admin;
+
+    private Integer sampleGroups;
+
+    private Integer quantityPerGroup;
+
+    private String inspector;
+
+    private String receivedDate;
+
+    private String sampleRequirement;
+
+    private String generation;
+
+    private String state;
+
+    private Date checkDate;
+
+    private String remark;
+
+    private String file;
+
+    private Integer productId;
+
+    private String outwardDescribe;
+
+    private String insertFlag;
+
+    private Integer isUse;
+
+    private String sampleQuantity;
+
+    private String aliasName;
+
+    private String sampleType;
+
+    private String sampleRemark;
+
+    private String picture;
+
+    public TestSampleEntity() {
+    }
+
+    public TestSampleEntity(SampleDetailAddVo vo,String sampleCode) {
+        this.companyId = vo.getCompanyId();
+        this.sampleName = vo.getAliasName();
+        this.sampleCode = sampleCode;
+        this.specs = vo.getSpecs();
+        this.batchNumber = vo.getBatchNumber();
+        this.manufacturer = vo.getManufacturer();
+        this.sampleOrigin = vo.getSampleOrigin();
+        this.outward = vo.getOutward();
+        this.inspector = vo.getInspector();
+        this.receivedDate = vo.getReceivedDate();
+        this.sampleRequirement = vo.getSampleRequirement();
+        this.generation = vo.getGeneration();
+        this.state = state;
+        this.productId = vo.getProductId();
+        this.outwardDescribe = vo.getOutwardDescribe();
+        this.isUse = 0;
+        this.sampleQuantity = vo.getSampleQuantity();
+        this.aliasName = vo.getAliasName();
+        this.sampleType = vo.getSampleType();
+        this.sampleRemark = vo.getSampleRemark();
+    }
+}
