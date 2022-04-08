464fbbf411e26de87bf5275b26a747d7b47e4ef5
gjl <18337165257@163.com>
2022-04-08 18:40:59 +0800

    token过期提示

--- src/main/java/com/lims/manage/erp/config/ShiroConfig.java
+++ src/main/java/com/lims/manage/erp/config/ShiroConfig.java
@@ -85,6 +85,7 @@ public class ShiroConfig {
 
         // 配置shiro默认登录界面地址，前后端分离中登录界面跳转应由前端路由控制，后台仅返回json数据
         shiroFilterFactoryBean.setLoginUrl("index.html");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/userLogin/unauth");
         filterChainDefinitionMap.put("/**", "authc");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return shiroFilterFactoryBean;
