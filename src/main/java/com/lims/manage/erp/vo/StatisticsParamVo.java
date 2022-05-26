3a4f3220ce5d15c93e7306633faed1662a49ccb1
psh <psh080808>
2022-05-26 16:07:10 +0800

    部门统计--子部门统计修改；

--- src/main/java/com/lims/manage/erp/vo/StatisticsParamVo.java
+++ src/main/java/com/lims/manage/erp/vo/StatisticsParamVo.java
@@ -7,7 +7,7 @@ public class StatisticsParamVo {
     private String taskSource;
     private String beginDate;
     private String endDate;
-
+    private String teamId;
     private Integer pageNum;
     private Integer pageSize;
 }
