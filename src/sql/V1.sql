a4f281c15e9791a9e6d5578272a1f0da21da7a76
丁连春 <1832164565@qq.com>
2022-06-01 16:27:57 +0800

    sql文件修改

--- src/sql/V1.sql
+++ src/sql/V1.sql
@@ -8,3 +8,18 @@ CREATE TABLE `test_alert` (
   `describ` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
+-- 6月1号
+ALTER TABLE sys_function  DROP kanban_name;
+
+ALTER TABLE sys_function ADD kanban_name VARCHAR(255);
+
+UPDATE sys_function set kanban_name = "待发布任务" WHERE function_id = 37;
+UPDATE sys_function set kanban_name = "待领取任务" WHERE function_id = 40;
+UPDATE sys_function set kanban_name = "试验检测中" WHERE function_id = 41;
+UPDATE sys_function set kanban_name = "待编制报告" WHERE function_id = 42;
+
+UPDATE sys_function set kanban_name = "待审核" WHERE function_id = 44;
+UPDATE sys_function set kanban_name = "待签发" WHERE function_id = 45;
+UPDATE sys_function set kanban_name = "待盖章" WHERE function_id = 46;
+UPDATE sys_function set kanban_name = "待发出报告" WHERE function_id = 47;
--- src/sql/my_shiro.sql
+++ src/sql/my_shiro.sql
@@ -108,18 +108,3 @@ INSERT INTO `sys_user_role` VALUES (1, 1457656933753110530, 1);
 INSERT INTO `sys_user_role` VALUES (2, 1457656966263160833, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
-
--- 6月1号
-ALTER TABLE sys_function  DROP kanban_name;
-
-ALTER TABLE sys_function ADD kanban_name VARCHAR(255);
-
-UPDATE sys_function set kanban_name = "待发布任务" WHERE function_id = 37;
-UPDATE sys_function set kanban_name = "待领取任务" WHERE function_id = 40;
-UPDATE sys_function set kanban_name = "试验检测中" WHERE function_id = 41;
-UPDATE sys_function set kanban_name = "待编制报告" WHERE function_id = 42;
-
-UPDATE sys_function set kanban_name = "待审核" WHERE function_id = 44;
-UPDATE sys_function set kanban_name = "待签发" WHERE function_id = 45;
-UPDATE sys_function set kanban_name = "待盖章" WHERE function_id = 46;
-UPDATE sys_function set kanban_name = "待发出报告" WHERE function_id = 47;
