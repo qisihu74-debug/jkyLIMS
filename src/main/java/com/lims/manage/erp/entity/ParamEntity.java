822a93ed803c8cd1d0deb0017b2cdb30d8174397
gjl <18337165257@163.com>
2022-07-06 17:03:58 +0800

    报告合并按照多组样品中的模板

---
+++ src/main/java/com/lims/manage/erp/entity/ParamEntity.java
@@ -0,0 +1,17 @@
+package com.lims.manage.erp.entity;
+
+import lombok.Data;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.entity
+ * @desc
+ * @date 2022/7/6 15:27
+ * @Copyright © 河南交科院
+ */
+@Data
+public class ParamEntity {
+    private int sampleId;
+    private String url;
+}
