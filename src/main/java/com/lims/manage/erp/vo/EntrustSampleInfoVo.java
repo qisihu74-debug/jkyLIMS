52646f42d7581491e543ad4880d50485171fec49
psh <psh080808>
2022-08-12 14:53:34 +0800

    委托单查询列表增加样品修改跳转功能；

--- src/main/java/com/lims/manage/erp/vo/EntrustSampleInfoVo.java
+++ src/main/java/com/lims/manage/erp/vo/EntrustSampleInfoVo.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class EntrustSampleInfoVo {
+    private Integer sampleId;
+    private String sampleType;
     private String aliasName;
     private String specs;
     private String outwardDescribe;
