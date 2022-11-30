ced40d3486960c549d9df1f5bfc9c7b229c2424c
psh <psh080808>
2022-11-30 08:47:10 +0800

    设备仪器管理--仪器使用记录查询；

--- src/main/java/com/lims/manage/erp/service/TestInstrumentService.java
+++ src/main/java/com/lims/manage/erp/service/TestInstrumentService.java
@@ -4,11 +4,14 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
 import com.lims.manage.erp.entity.TestInstrument;
 import com.lims.manage.erp.entity.TestLaboratory;
 import com.lims.manage.erp.result.Result;
+import com.lims.manage.erp.vo.InstrumentRecordParamVo;
 import com.lims.manage.erp.vo.TestInstrumentVo;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,5 +25,12 @@ public interface TestInstrumentService extends IService<TestInstrument> {
     Result updInstrument(TestInstrument testInstrument);
     Result delInstruments(List<Long> idList);
     IPage<TestInstrumentVo> getPageList(Page<TestInstrumentVo> page, QueryWrapper<TestInstrument> queryWrapper);
+
+    /**
+     * 查询设备使用记录
+     * @param paramVo
+     * @return
+     */
+    PageInfo getInstrumentRecord(InstrumentRecordParamVo paramVo);
 }
 
---
+++ src/main/java/com/lims/manage/erp/vo/InstrumentRecordParamVo.java
@@ -0,0 +1,22 @@
+package com.lims.manage.erp.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+public class InstrumentRecordParamVo {
+    private String taskCode;
+    private String instrumentCode;
+    private String instrumentName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date endTime;
+    private Integer pageNum;
+    private Integer pageSize;
+}
