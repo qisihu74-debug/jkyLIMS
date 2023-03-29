b6ae24b68b6eed38b1f7d939262f80217793a21e
gjl <18337165257@163.com>
2023-03-29 17:52:11 +0800

    仪器设备标识卡下载打印

---
+++ src/main/java/com/lims/manage/erp/controller/AppDeviceController.java
@@ -0,0 +1,19 @@
+package com.lims.manage.erp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.controller
+ * @desc
+ * @date 2023-03-29 9:37
+ * @Copyright © 河南交科院
+ */
+@RestController
+@RequestMapping("/app/")
+public class AppDeviceController {
+
+
+}
