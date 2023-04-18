e24fc6d59499422fb42d968764d21b585b315c48
gjl <18337165257@163.com>
2023-04-18 11:04:45 +0800

    引入pageOffice 2023-04-18

--- src/main/java/com/lims/manage/erp/MyApplication.java
+++ src/main/java/com/lims/manage/erp/MyApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableMBeanExport;
@@ -20,6 +21,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.FileNotFoundException;
 
 /**
  * @author gjl
@@ -38,15 +40,14 @@ public class MyApplication {
     private int serverPort;
     @Value("${http.port}")
     private int httpPort;
+    @Value("${posyspath}")
+    private String poSysPath;
+    @Value("${popassword}")
+    private String poPassword;
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
-
-    /*@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MyApplication.class);
-    }*/
 
     /**
      * SpringBoot2.x配置HTTPS,并实现HTTP访问自动转向HTTPS
@@ -75,23 +76,7 @@ public class MyApplication {
         tomcat.addAdditionalTomcatConnectors(httpConnector());
         return tomcat;
     }
-    /**
-     * 解决异常信息：
-     *  java.lang.IllegalArgumentException:
-     *      Invalid character found in the request target. The valid characters are defined in RFC 7230 and RFC 3986
-     * @return
-     */
-    /*@Bean
-    public ConfigurableServletWebServerFactory webServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-            @Override
-            public void customize(Connector connector) {
-                connector.setProperty("relaxedQueryChars", "|{}[]\\");
-            }
-        });
-        return factory;
-    }*/
+
     @Bean
     public Connector httpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
@@ -116,5 +101,46 @@ public class MyApplication {
         /// 设置总上传数据总大小
         factory.setMaxRequestSize("409600KB");
         return factory.createMultipartConfig();
+    }
+
+    /**
+     * 添加PageOffice的服务器端授权程序Servlet（必须）
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean pageofficeRegistrationBean() {
+        com.zhuozhengsoft.pageoffice.poserver.Server poserver = new com.zhuozhengsoft.pageoffice.poserver.Server();
+        poserver.setSysPath(poSysPath);//设置PageOffice注册成功后,license.lic文件存放的目录
+        ServletRegistrationBean srb = new ServletRegistrationBean(poserver);
+        srb.addUrlMappings("/poserver.zz");
+        srb.addUrlMappings("/posetup.exe");
+        srb.addUrlMappings("/pageoffice.js");
+        srb.addUrlMappings("/jquery.min.js");
+        srb.addUrlMappings("/pobstyle.css");
+        srb.addUrlMappings("/sealsetup.exe");
+        return srb;//
+    }
+
+
+    /**
+     * 添加印章管理程序Servlet（可选）
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean zoomsealRegistrationBean() throws FileNotFoundException {
+        com.zhuozhengsoft.pageoffice.poserver.AdminSeal adminSeal = new com.zhuozhengsoft.pageoffice.poserver.AdminSeal();
+        adminSeal.setAdminPassword(poPassword);//设置印章管理员admin的登录密码（为了安全起见，强烈建议修改此密码）
+        /**如果当前项目是打成jar或者war包运行，强烈建议将poseal.db文件的路径更换成某个固定的绝对路径下,不要放当前项目文件夹下,为了防止每次重新打包程序导致poseal.db被替换的问题。
+         * 比如windows服务器下：D:/lic/，linux服务器下:/root/lic/
+         */
+        //设置印章数据库文件poseal.db存放的目录
+        adminSeal.setSysPath(poSysPath);
+        ServletRegistrationBean srb = new ServletRegistrationBean(adminSeal);
+        srb.addUrlMappings("/adminseal.zz");
+        srb.addUrlMappings("/sealimage.zz");
+        srb.addUrlMappings("/loginseal.zz");
+        return srb;//
     }
 }
---
+++ src/main/java/com/lims/manage/erp/entity/SaveParamBean.java
@@ -0,0 +1,15 @@
+package com.lims.manage.erp.entity;
+
+import lombok.Data;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.entity
+ * @desc
+ * @date 2023-04-18 11:02
+ * @Copyright © 河南交科院
+ */
+@Data
+public class SaveParamBean {
+}
