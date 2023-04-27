2790a01862c63ac85b621c04c524fc59ed7ec35d
丁连春 <1832164565@qq.com>
2023-04-27 11:26:22 +0800

    根据token存放用户和返回用户信息

--- src/main/java/com/lims/manage/erp/util/RedisUtil.java
+++ src/main/java/com/lims/manage/erp/util/RedisUtil.java
@@ -1,5 +1,8 @@
 package com.lims.manage.erp.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.lims.manage.erp.entity.SysUserEntity;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,13 +25,45 @@ public class RedisUtil {
                 return null;
             }
             Object o = redisUtils.get("shiro:session:" + token);
-            if (o == null){
+            if (o == null) {
                 return null;
-            }else {
+            } else {
                 return token;
             }
         } else {
             return null;
         }
     }
+
+    /**
+     * 根据 token 存储 用户信息
+     *
+     * @param token
+     * @param userData
+     * @return
+     */
+    public Boolean setRedisTokenUser(String token, SysUserEntity userData) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user", userData);
+        // 过期时间 2个小时
+        redisUtils.set(token, jsonObject.toJSONString(), 7200);
+        return true;
+    }
+
+    /**
+     * 根据 token 返回 用户信息
+     * @param token
+     * @return
+     */
+    public SysUserEntity getRedisTokenUser(String token) {
+        // 过期时间 2个小时
+        String jsonObject = (String) redisUtils.get(token);
+        if (jsonObject == null) {
+            return null;
+        }
+        JSONObject object = (JSONObject) JSONObject.parse(jsonObject);
+        return JSONObject.toJavaObject((JSON) object.get("user"), SysUserEntity.class);
+    }
+
+
 }
