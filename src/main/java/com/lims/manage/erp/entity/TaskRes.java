e29e6b796ef40c2ba2ddb75753ab42a212fe20d3
gjl <18337165257@163.com>
2023-10-10 19:29:03 +0800

    我的任务

---
+++ src/main/java/com/lims/manage/erp/entity/TaskRes.java
@@ -0,0 +1,17 @@
+package com.lims.manage.erp.entity;
+
+import lombok.Data;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.entity
+ * @desc
+ * @date 2023-10-10 19:19
+ * @Copyright © 河南交科院
+ */
+@Data
+public class TaskRes {
+    private String taskCode;
+    private String id;
+}
