bade80a835d502f8694a5910a32bfa9e490b6c28
gjl <18337165257@163.com>
2024-01-25 15:12:47 +0800

    20240125

---
+++ src/main/java/com/lims/manage/erp/entity/JsonRootBean.java
@@ -0,0 +1,383 @@
+package com.lims.manage.erp.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+ /**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.entity
+ * @desc
+ * @date 2024-01-25 11:18
+ * @Copyright © 河南交科院
+ */
+ @Data
+ @TableName("heat_exchanger")
+public class JsonRootBean {
+
+    private String namech;
+    private String a104cn;
+    private String a847;
+    private String a209;
+    private String a100_sname;
+    private String id;
+    private String href;
+    private String a800;
+    private String a001;
+    private String a330_c;
+    private String solrfq;
+    private String active;
+    private String a825_en;
+    private Date shishidate;
+    private Date fabudate;
+    private String filename_json_s;
+    private String a104new;
+    private String a871;
+    private String biaoz_en;
+    private String cnstandcategory;
+    private String a461;
+    private String a462;
+    private String description;
+    private String a826_en;
+    private String nameen;
+    private String cycle;
+    private String pagenumber;
+    private String zuofeidate;
+    private String a104en;
+    private String fulltext_s;
+    private String a298_c;
+    private String sub_type;
+    private String a825;
+    private String a826;
+    private String a104;
+    private String sima298_s;
+    private String a502;
+    private String classfiy;
+    private String filename_s;
+    private String confirmdate;
+    private String a330;
+    private String photo;
+    private String personofdraft;
+    private String fanwei;
+    private String biaoz;
+    private String interstandcategory;
+    //private List<Pricebeans> pricebeans;
+    public void setNamech(String namech) {
+        this.namech = namech;
+    }
+    public String getNamech() {
+        return namech;
+    }
+
+    public void setA104cn(String a104cn) {
+        this.a104cn = a104cn;
+    }
+    public String getA104cn() {
+        return a104cn;
+    }
+
+    public void setA847(String a847) {
+        this.a847 = a847;
+    }
+    public String getA847() {
+        return a847;
+    }
+
+    public void setA209(String a209) {
+        this.a209 = a209;
+    }
+    public String getA209() {
+        return a209;
+    }
+
+    public void setA100_sname(String a100_sname) {
+        this.a100_sname = a100_sname;
+    }
+    public String getA100_sname() {
+        return a100_sname;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+    public String getHref() {
+        return href;
+    }
+
+    public void setA800(String a800) {
+        this.a800 = a800;
+    }
+    public String getA800() {
+        return a800;
+    }
+
+    public void setA001(String a001) {
+        this.a001 = a001;
+    }
+    public String getA001() {
+        return a001;
+    }
+
+    public void setA330_c(String a330_c) {
+        this.a330_c = a330_c;
+    }
+    public String getA330_c() {
+        return a330_c;
+    }
+
+    public void setSolrfq(String solrfq) {
+        this.solrfq = solrfq;
+    }
+    public String getSolrfq() {
+        return solrfq;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+    public String getActive() {
+        return active;
+    }
+
+    public void setA825_en(String a825_en) {
+        this.a825_en = a825_en;
+    }
+    public String getA825_en() {
+        return a825_en;
+    }
+
+    public void setShishidate(Date shishidate) {
+        this.shishidate = shishidate;
+    }
+    public Date getShishidate() {
+        return shishidate;
+    }
+
+    public void setFabudate(Date fabudate) {
+        this.fabudate = fabudate;
+    }
+    public Date getFabudate() {
+        return fabudate;
+    }
+
+    public void setFilename_json_s(String filename_json_s) {
+        this.filename_json_s = filename_json_s;
+    }
+    public String getFilename_json_s() {
+        return filename_json_s;
+    }
+
+    public void setA104new(String a104new) {
+        this.a104new = a104new;
+    }
+    public String getA104new() {
+        return a104new;
+    }
+
+    public void setA871(String a871) {
+        this.a871 = a871;
+    }
+    public String getA871() {
+        return a871;
+    }
+
+    public void setBiaoz_en(String biaoz_en) {
+        this.biaoz_en = biaoz_en;
+    }
+    public String getBiaoz_en() {
+        return biaoz_en;
+    }
+
+    public void setCnstandcategory(String cnstandcategory) {
+        this.cnstandcategory = cnstandcategory;
+    }
+    public String getCnstandcategory() {
+        return cnstandcategory;
+    }
+
+    public void setA461(String a461) {
+        this.a461 = a461;
+    }
+    public String getA461() {
+        return a461;
+    }
+
+    public void setA462(String a462) {
+        this.a462 = a462;
+    }
+    public String getA462() {
+        return a462;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setA826_en(String a826_en) {
+        this.a826_en = a826_en;
+    }
+    public String getA826_en() {
+        return a826_en;
+    }
+
+    public void setNameen(String nameen) {
+        this.nameen = nameen;
+    }
+    public String getNameen() {
+        return nameen;
+    }
+
+    public void setCycle(String cycle) {
+        this.cycle = cycle;
+    }
+    public String getCycle() {
+        return cycle;
+    }
+
+    public void setPagenumber(String pagenumber) {
+        this.pagenumber = pagenumber;
+    }
+    public String getPagenumber() {
+        return pagenumber;
+    }
+
+    public void setZuofeidate(String zuofeidate) {
+        this.zuofeidate = zuofeidate;
+    }
+    public String getZuofeidate() {
+        return zuofeidate;
+    }
+
+    public void setA104en(String a104en) {
+        this.a104en = a104en;
+    }
+    public String getA104en() {
+        return a104en;
+    }
+
+    public void setFulltext_s(String fulltext_s) {
+        this.fulltext_s = fulltext_s;
+    }
+    public String getFulltext_s() {
+        return fulltext_s;
+    }
+
+    public void setA298_c(String a298_c) {
+        this.a298_c = a298_c;
+    }
+    public String getA298_c() {
+        return a298_c;
+    }
+
+    public void setSub_type(String sub_type) {
+        this.sub_type = sub_type;
+    }
+    public String getSub_type() {
+        return sub_type;
+    }
+
+    public void setA825(String a825) {
+        this.a825 = a825;
+    }
+    public String getA825() {
+        return a825;
+    }
+
+    public void setA826(String a826) {
+        this.a826 = a826;
+    }
+    public String getA826() {
+        return a826;
+    }
+
+    public void setA104(String a104) {
+        this.a104 = a104;
+    }
+    public String getA104() {
+        return a104;
+    }
+
+    public void setSima298_s(String sima298_s) {
+        this.sima298_s = sima298_s;
+    }
+    public String getSima298_s() {
+        return sima298_s;
+    }
+
+    public void setA502(String a502) {
+        this.a502 = a502;
+    }
+    public String getA502() {
+        return a502;
+    }
+
+    public void setClassfiy(String classfiy) {
+        this.classfiy = classfiy;
+    }
+    public String getClassfiy() {
+        return classfiy;
+    }
+
+    public void setFilename_s(String filename_s) {
+        this.filename_s = filename_s;
+    }
+    public String getFilename_s() {
+        return filename_s;
+    }
+
+    public void setConfirmdate(String confirmdate) {
+        this.confirmdate = confirmdate;
+    }
+    public String getConfirmdate() {
+        return confirmdate;
+    }
+
+    public void setA330(String a330) {
+        this.a330 = a330;
+    }
+    public String getA330() {
+        return a330;
+    }
+
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPersonofdraft(String personofdraft) {
+        this.personofdraft = personofdraft;
+    }
+    public String getPersonofdraft() {
+        return personofdraft;
+    }
+
+    public void setFanwei(String fanwei) {
+        this.fanwei = fanwei;
+    }
+    public String getFanwei() {
+        return fanwei;
+    }
+
+    public void setBiaoz(String biaoz) {
+        this.biaoz = biaoz;
+    }
+    public String getBiaoz() {
+        return biaoz;
+    }
+
+    public void setInterstandcategory(String interstandcategory) {
+        this.interstandcategory = interstandcategory;
+    }
+    public String getInterstandcategory() {
+        return interstandcategory;
+    }
+
+}
