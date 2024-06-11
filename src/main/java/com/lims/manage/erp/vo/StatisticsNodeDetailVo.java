084897fba951aafa95f6b124787aa73d5d916ccd
dlc <1832164566@qq.com>
2024-06-11 14:48:29 +0800

    创建类提交

---
+++ src/main/java/com/lims/manage/erp/vo/StatisticsNodeDetailVo.java
@@ -0,0 +1,44 @@
+package com.lims.manage.erp.vo;
+
+import lombok.Data;
+
+/**
+ * @Author: DLC
+ * @Date: 2024/6/11 11:10
+ * 报告清单详情表
+ */
+@Data
+public class StatisticsNodeDetailVo {
+    /**
+     * id
+     */
+    private Long id;
+    /**
+     * 委托单id
+     */
+    private String entrustmentId;
+    /**
+     * 报告号
+     */
+    private String reportCode;
+    /**
+     * 样品名称
+     */
+    private String sampleName;
+    /**
+     * 折扣率
+     */
+    private String discount;
+    /**
+     * 应收价格
+     */
+    private String actualPrice;
+    /**
+     * 实际收费
+     */
+    private String systemPrice;
+    /**
+     * 委托单号
+     */
+    private String entrustmentNo;
+}
