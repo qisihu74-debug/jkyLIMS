d2871e5b1bad370051638614ba2279551e120830
zhq <1325758778@qq.com>
2024-06-21 09:35:45 +0800

    产品删除处理

--- src/main/java/com/lims/manage/erp/service/impl/TestProductTypeServiceImpl.java
+++ src/main/java/com/lims/manage/erp/service/impl/TestProductTypeServiceImpl.java
@@ -1,17 +1,23 @@
 package com.lims.manage.erp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lims.manage.erp.entity.SysUserEntity;
+import com.lims.manage.erp.entity.TestProduct;
 import com.lims.manage.erp.mapper.TestProductTypeDao;
 import com.lims.manage.erp.entity.TestProductType;
 import com.lims.manage.erp.result.Result;
 import com.lims.manage.erp.result.ResultUtil;
 import com.lims.manage.erp.service.LogManagerService;
+import com.lims.manage.erp.service.TestProductService;
 import com.lims.manage.erp.service.TestProductTypeService;
 import com.lims.manage.erp.util.Const;
 import com.lims.manage.erp.util.ShiroUtils;
+import com.lims.manage.erp.util.StringUtils;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.ListUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -28,6 +34,10 @@ import java.util.List;
 public class TestProductTypeServiceImpl extends ServiceImpl<TestProductTypeDao, TestProductType> implements TestProductTypeService {
     @Resource
     private LogManagerService logManagerService;
+
+    @Resource
+    private TestProductService testProductService;
+
     @Override
     public Result addProductType(TestProductType type) {
         SysUserEntity userInfo = ShiroUtils.getUserInfo();
@@ -84,16 +94,39 @@ public class TestProductTypeServiceImpl extends ServiceImpl<TestProductTypeDao, 
             return ResultUtil.error("token 已过期！");
         }
         List<TestProductType> testProductTypeList=new ArrayList<>();
+        //存储有产品的产品分类名称
+        StringBuilder noDelProductType=new StringBuilder();
         for (Long aLong : idList) {
-            TestProductType testProductType=new TestProductType();
-            testProductType.setUpdateTime(new Date());
-            testProductType.setDelFlag(1);
-            testProductType.setProductTypeId(aLong.intValue());
-            testProductTypeList.add(testProductType);
+            //根据产品分类id获取产品信息
+            LambdaQueryWrapper<TestProduct> wrapper= Wrappers.lambdaQuery();
+            wrapper.eq(TestProduct::getProductTypeId,aLong);
+            int count = testProductService.count(wrapper);
+            if(count==0){
+                TestProductType testProductType=new TestProductType();
+                testProductType.setUpdateTime(new Date());
+                testProductType.setDelFlag(1);
+                testProductType.setProductTypeId(aLong.intValue());
+                testProductTypeList.add(testProductType);
+            }else{
+                TestProductType productType = this.getById(aLong);
+                if(productType!=null){
+                    noDelProductType.append(productType.getProductTypeName()).append(",");
+                }
+            }
+        }
+        if(ListUtils.isEmpty(testProductTypeList)){
+            if(StringUtils.isNotEmpty(noDelProductType)){
+                return ResultUtil.error("产品分类："+noDelProductType+"下存在产品,请删除产品后再尝试！");
+            }else{
+                return ResultUtil.error("没有找到要删除的产品分类信息");
+            }
         }
         String idStr=idList.toString();
         if (this.updateBatchById(testProductTypeList)){
             logManagerService.addOpSysLog(ShiroUtils.getUserInfo(),"用户："+userInfo.getUsername()+"删除产品类型"+idStr+"成功!", Const.PRODUCT_MANAGEMENT_LOG,true);
+            if(StringUtils.isNotEmpty(noDelProductType)){
+                return ResultUtil.error("产品分类："+noDelProductType+"下存在产品,请删除产品后再尝试！");
+            }
             return ResultUtil.success("删除成功");
         }else {
             logManagerService.addOpSysLog(ShiroUtils.getUserInfo(),"用户："+userInfo.getUsername()+"删除产品类型"+idStr+"失败!", Const.PRODUCT_MANAGEMENT_LOG,false);
