0e913575840820dcea86fb4c557bcd124895027e
gjl <18337165257@163.com>
2024-07-04 15:59:52 +0800

    内审

---
+++ src/main/java/com/lims/manage/erp/controller/QsAuditController.java
@@ -0,0 +1,18 @@
+package com.lims.manage.erp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gjl
+ * @version V1.0
+ * @Package com.lims.manage.erp.controller
+ * @desc
+ * @date 2024-07-04 15:56
+ * @Copyright © 河南交科院
+ */
+@RestController
+@RequestMapping("/audit/")
+public class QsAuditController {
+
+}
