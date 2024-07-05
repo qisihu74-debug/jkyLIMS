f57f918020f3eaba6af1dada32fa576b52b6acb2
gjl <18337165257@163.com>
2024-07-05 10:15:01 +0800

    内审

--- src/main/java/com/lims/manage/erp/controller/QsAuditController.java
+++ src/main/java/com/lims/manage/erp/controller/QsAuditController.java
@@ -1,5 +1,8 @@
 package com.lims.manage.erp.controller;
 
+import com.lims.manage.erp.annotation.Log;
+import com.lims.manage.erp.result.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @author gjl
  * @version V1.0
  * @Package com.lims.manage.erp.controller
- * @desc
+ * @desc 部门负责人（部长/所长/主任）；内审员（具有内审角色的账户）
  * @date 2024-07-04 15:56
  * @Copyright © 河南交科院
  */
 @RestController
 @RequestMapping("/audit/")
 public class QsAuditController {
+
+    /**
+     * 技术质量部内审活动列表
+     * @param name
+     * @param time
+     * @return
+     */
+    @GetMapping("internalAuditorActiveList")
+    public Result internalAuditorActiveList(String name,String time){
+
+
+        return null;
+    }
 
 }
