9403aeae5c8b6c6d065c02fb582010fda0d4df2f
dlc <1832164566@qq.com>
2024-07-05 17:20:42 +0800

    工时更新

---
+++ src/main/java/com/lims/manage/erp/service/impl/TestItemOrderWorkingHoursServiceImpl.java
@@ -0,0 +1,16 @@
+package com.lims.manage.erp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lims.manage.erp.entity.TestItemOrderWorkingHours;
+import com.lims.manage.erp.mapper.TestItemOrderWorkingHoursMapper;
+import com.lims.manage.erp.service.TestItemOrderWorkingHoursService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: DLC
+ * @Date: 2024/7/5 11:56
+ */
+@Service
+public class TestItemOrderWorkingHoursServiceImpl extends ServiceImpl<TestItemOrderWorkingHoursMapper, TestItemOrderWorkingHours> implements TestItemOrderWorkingHoursService {
+
+}
---
+++ src/main/java/com/lims/manage/erp/service/impl/TestTaskOrderWorkingHoursServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lims.manage.erp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lims.manage.erp.entity.TestTaskOrderWorkingHours;
+import com.lims.manage.erp.mapper.TestTaskOrderWorkingHoursMapper;
+import com.lims.manage.erp.service.TestTaskOrderWorkingHoursService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: DLC
+ * @Date: 2024/7/5 11:58
+ */
+@Service
+public class TestTaskOrderWorkingHoursServiceImpl extends ServiceImpl<TestTaskOrderWorkingHoursMapper, TestTaskOrderWorkingHours> implements TestTaskOrderWorkingHoursService {
+}
