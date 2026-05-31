ddaf82fed321bd7d602b0416fd2c5bcdc00a3a26
qisihu74 <qisihu74@gmail.com>
2026-05-31 22:21:54 +0800

    fix(异常处理): DefaultControllerAdvice 记录未捕获异常(原静默吞掉无日志)

--- src/main/java/com/lims/manage/erp/config/DefaultControllerAdvice.java
+++ src/main/java/com/lims/manage/erp/config/DefaultControllerAdvice.java
@@ -3,23 +3,24 @@ package com.lims.manage.erp.config;
 import com.lims.manage.erp.result.Result;
 import com.lims.manage.erp.result.ResultEnum;
 import com.lims.manage.erp.result.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 /**
  * @author gjl
  */
-
-
 @ControllerAdvice
 @ResponseBody
 public class DefaultControllerAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public Result exceptionHandler() {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultControllerAdvice.class);
 
-        return ResultUtil.error(ResultEnum.UNKNOWN_ERROR.getCode(),ResultEnum.UNKNOWN_ERROR.getMsg());
+    @ExceptionHandler(Exception.class)
+    public Result exceptionHandler(Exception e) {
+        logger.error("[DefaultControllerAdvice] 未捕获异常: ", e);
+        return ResultUtil.error(ResultEnum.UNKNOWN_ERROR.getCode(), ResultEnum.UNKNOWN_ERROR.getMsg());
     }
 }
